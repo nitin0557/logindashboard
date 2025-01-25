94f6a7c9f5bd4b915e4448a93bb5de995b7dfb58
nit789 <nitinkrsingh789@gmail.com>
2025-01-25 23:19:46 +0530

    new commit

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, "public"),
+    path: path.join(__dirname, "build"),
     filename: "bundle.js",
   },
 
