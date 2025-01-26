353b002e352f2fe9427d2bf5d70ddfb062ebe292
nit789 <nitinkrsingh789@gmail.com>
2025-01-26 11:11:34 +0530

    new build

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, "newbuild"),
+    path: path.join(__dirname, "public"),
     filename: "bundle.js",
   },
 
